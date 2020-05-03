a21be1b5d5e04dc0f791cf102d6b7363f03df048
sriramede9 <only4programming03@gmail.com>
2020-05-03 11:41:20 +0100

    added how to get route params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
 
-  constructor() { }
+  constructor(private routes: ActivatedRoute) {}
 
   ngOnInit() {
+    this.user = {
+      id: this.routes.snapshot.params["id"],
+      name: this.routes.snapshot.params["name"],
+    };
   }
-
 }
